7bab544705c79b327d2c63d043bf5526fabd0f12
542101079-hub <542101079@qq.com>
2025-09-26 16:43:17 +0800

    UI:报错修改好

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // Allow Turbopack to load the CommonJS bcryptjs package on the server.
+    serverComponentsExternalPackages: ["bcryptjs"],
+  },
 };
 
 export default nextConfig;
